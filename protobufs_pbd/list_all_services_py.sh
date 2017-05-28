2160f40702aacc7a637efb9f57bd725c3c759d66
Henri Chain <henri.chain@eleves.ec-nantes.fr>
2017-05-28 17:39:21 +0200

    rename

--- protobufs_pbd/list_all_services_py.sh
+++ protobufs_pbd/list_all_services_py.sh
@@ -2,20 +2,20 @@ imports=()
 echo 'all_services = ['
 for file in $(find -name '*.proto'); do
 	package=$(cat $file|sed -En 's/^package (.*);/\1/p')
-	#cat $file|sed -En "s/^message (.*)\{/${package}.\1/p"
+	#cat $file|sed -En "s/^service (.*)\{/${package}.\1/p"
 	importname=${file%%.proto}
 	importname=${importname##./}
 	importname=${importname//\//.}_pb2
 	#echo "import $importname"
 	imports+=("$importname")
-	messages=$(cat $file|sed -En "s/^service (.*)\{/\1/p")
+	services=$(cat $file|sed -En "s/^service (.*)\{/\1/p")
 	
-	for message in $messages; do
+	for service in $services; do
 		echo "	{
-		\"msg\": ${importname}.${message}(),
-		\"name\": \"${package}.${message}\",
+		\"srv\": ${importname}.${service}(),
+		\"name\": \"${package}.${service}\",
 	},"
-		#echo $message|
+		#echo $service|
 		#sed -E 's/(.)([A-Z])/\1_\2/g' |
 		#tr 'A-Z' 'a-z'
 	done
