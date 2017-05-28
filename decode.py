76fd746c974890d3ffea700e3801b377a2ccd15a
Henri Chain <henri.chain@eleves.ec-nantes.fr>
2017-05-28 12:15:23 +0200

    make decoder generic

--- decode.py
+++ decode.py
@@ -6,111 +6,114 @@ import subprocess
 from pprint import pprint
 import pickle
 
-magics = [0xc2, 0xc3]
+class DevialetDecoder:
+    def __init__(self):
+        self.magics = [0xc2, 0xc3]
+        self.magic_headers = [b'\xc2\x01\x00\x00\x00\x00', b'\xc3\x01\x00\x00\x00\x00']
 
-def decode_protobuf(data):
-    process = subprocess.Popen(['protoc', '--decode_raw'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    process.stdin.write(data)
-    process.stdin.close()
-    ret = process.wait()
-    if ret == 0:
-        return process.stdout.read().decode()
-    else:
-        return 'Failed to parse: ' + ' '.join('{:02x}'.format(x) for x in data)
+    def decode_protobuf(self, data):
+        process = subprocess.Popen(['protoc', '--decode_raw'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        process.stdin.write(data)
+        process.stdin.close()
+        ret = process.wait()
+        if ret == 0:
+            return {
+                "raw": data,
+                "protoc": process.stdout.read().decode()
+            }
+        else:
+            return {
+                "raw": data,
+                "protoc": "",
+                "error": 'Failed to parse: ' + ' '.join('{:02x}'.format(x) for x in data)
+            }
 
-def decode_section(section):
-    obj = {'magic': section[0]['firstbyte']} #{'fields': [field.hex() for field in section]}
+    def decode_section(self, section):
+        obj = {
+            'magic': section[0]['firstbyte'],
+            # 'raw_fields': section
+        }
 
-    # Delimiter, always empty
-    i = 0
-    if len(section[i]['data']) != 0:
-        obj.setdefault('error', []).append('Weird, field {}/{} of length {} instead of {}'.format(
-            i, len(section), len(section[i]['data']), 0))
-    i += 1
-
-    # Extra field (+ delimiter) that doesnt decode into protobuf, some kind of uid?
-    if len(section) == 5 and len(section[i]['data']) == 16 and len(section[i+1]['data']) == 0:
-        obj['uid'] = section[i]['data'].hex()
-        i += 2
-    
-    # Remaining fields should all be protobufs
-    while i < len(section):
-        obj.setdefault('protobufs', []).append(decode_protobuf(section[i]['data']))
-        if section[i]['firstbyte'] != obj['magic']:
-            obj.setdefault('error', []).append('Weird, magic mismatch {} != {}'.format(
-                section[i]['firstbyte'], obj['magic']))
+        # Delimiter, always empty
+        i = 0
+        if len(section[i]['data']) != 0:
+            obj.setdefault('error', []).append('Weird, field {}/{} of length {} instead of {}'.format(
+                i, len(section), len(section[i]['data']), 0))
         i += 1
 
-    return obj
+        # Extra field (+ delimiter) that doesnt decode into protobuf, some kind of uid?
+        if len(section) == 5 and len(section[i]['data']) == 16 and len(section[i+1]['data']) == 0:
+            obj['uid'] = section[i]['data'].hex()
+            i += 2
+        
+        # Remaining fields should all be protobufs
+        while i < len(section):
+            obj.setdefault('protobufs', []).append(self.decode_protobuf(section[i]['data']))
+            if section[i]['firstbyte'] != obj['magic']:
+                obj.setdefault('error', []).append('Weird, magic mismatch {} != {}'.format(
+                    section[i]['firstbyte'], obj['magic']))
+            i += 1
 
-def decode_packet(packet):
-    field_header = packet.read(6)
-    firstbyte = field_header[0]
-    new_section = True
-    sections = []
-    ret = {'decoded': []}
+        return obj
 
-    while len(field_header) == 6 and firstbyte in magics:
-        firstbyte, same_section, field_length = struct.unpack('>BBL', field_header)
-        field_data = packet.read(field_length)
-        field = { 'firstbyte': firstbyte, 'data': field_data }
-
-        if new_section:
-            sections.append([field])
-        else:
-            sections[-1].append(field)           
-
+    def decode_packet(self, packet):
         field_header = packet.read(6)
-        # when second byte of section header is 0, next field is part of new section
-        new_section = not same_section
+        firstbyte = field_header[0]
+        new_section = True
+        sections = []
+        ret = {'sections': []}
 
-    rest = packet.read()
-    if len(field_header) != 0 or firstbyte not in magics or rest:
-         ret.setdefault('error', []).append('Found garbage at end of packet: header={} rest={}'.format(
-            field_header.hex(), ' '.join('{:02x}'.format(x) for x in rest)))
+        while len(field_header) == 6 and firstbyte in self.magics:
+            firstbyte, same_section, field_length = struct.unpack('>BBL', field_header)
+            field_data = packet.read(field_length)
+            field = { 'firstbyte': firstbyte, 'data': field_data }
 
-    for section in sections:
-        ret['decoded'].append(decode_section(section))
+            if new_section:
+                sections.append([field])
+            else:
+                sections[-1].append(field)           
 
-    return ret
+            field_header = packet.read(6)
+            # when second byte of section header is 0, next field is part of new section
+            new_section = not same_section
 
-def is_whole_packet(packet):
-    field_header = packet.read(6)
-    firstbyte = field_header[0]
+        rest = packet.read()
+        if len(field_header) != 0 or firstbyte not in self.magics or rest:
+             ret.setdefault('error', []).append('Found garbage at end of packet: header={} rest={}'.format(
+                field_header.hex(), ' '.join('{:02x}'.format(x) for x in rest)))
 
-    while len(field_header) == 6 and firstbyte in magics:
-        firstbyte, same_section, field_length = struct.unpack('>BBL', field_header)
-        field = packet.read(field_length)
+        for section in sections:
+            ret['sections'].append(self.decode_section(section))
 
+        return ret
+
+    def is_whole_packet(self, packet):
         field_header = packet.read(6)
+        firstbyte = field_header[0]
 
-    return (field_length == len(field) and len(field_header) == 0)
+        while len(field_header) == 6 and firstbyte in self.magics:
+            firstbyte, same_section, field_length = struct.unpack('>BBL', field_header)
+            field = packet.read(field_length)
 
-def decode_capture(filename):
-    with open(filename, 'rb') as file:
-        header = file.read(16)
+            field_header = packet.read(6)
+
+        return (field_length == len(field) and len(field_header) == 0)
+
+    def decode_capture(self, iter):
         packets = []
         last_packet_whole = True
+        print('.')
 
-        while len(header) == 16:
-            tstamp, direction, pad1, pad2, length, pad3 = struct.unpack('<qBBHhH', header)
-            packet = {
-                'time': datetime.fromtimestamp(tstamp / 1000),
-                'direction': direction, #'->' if direction else '<-'
-                'number': len(packets),
-                'length': length,
-                'merged': 1,
-                'data': file.read(length)
-            }
+        for packet in iter:
             # print(packet['direction'], packet['data'].hex())
             if not last_packet_whole:
                 packets[-1]['data'] += packet['data']
                 packets[-1]['merged'] += 1
-                last_packet_whole = is_whole_packet(BytesIO(packets[-1]['data']))
+                last_packet_whole = self.is_whole_packet(BytesIO(packets[-1]['data']))
 
-            elif packet['data'][:6] in [b'\xc2\x01\x00\x00\x00\x00', b'\xc3\x01\x00\x00\x00\x00']:
+            elif packet['data'][:6] in self.magic_headers:
                 packets.append(packet)
-                last_packet_whole = is_whole_packet(BytesIO(packet['data']))
+                last_packet_whole = self.is_whole_packet(BytesIO(packet['data']))
 
             else:
                 packet['error_unknown'] = 'Unknown packet {}'.format(
@@ -118,36 +121,60 @@ def decode_capture(filename):
                 packets.append(packet)
                 last_packet_whole = True
 
-            header = file.read(16)
-
         for packet in packets:
-            packet.update(decode_packet(BytesIO(packet.pop('data'))))
+            packet.update(self.decode_packet(BytesIO(packet.pop('data'))))
 
         return packets
 
-def decode_session(dirname):
-    session = {
-        'name': dirname,
-        'captures': [{
-            'name': capture,
-            'port': re.match('^[0-9]+_([0-9]+).dat$', capture).group(1),
-            'start_time': datetime.fromtimestamp(int(re.match('^([0-9]+)_[0-9]+.dat$', capture).group(1))/1000),
-            'packets': decode_capture(os.path.join(dirname, capture))
-        } for capture in sorted(os.listdir(dirname))]
-    }
+    def decode_sessions(self):
+        raise NotImplementedError()
 
-    session['start_time'] = min(capture['start_time'] for capture in session['captures'])
-    session['number_of_captures'] = len(session['captures'])
+class AndroidCaptureDecoder(DevialetDecoder):
+    def __init__(self, dirs):
+        DevialetDecoder.__init__(self)
+        self.dirs = dirs
 
-    return session
+    def capture_iter(self, filename):
+        i = 0
+        with open(filename, 'rb') as file:
+            header = file.read(16)
+            while len(header) == 16:
+                tstamp, direction, pad1, pad2, length, pad3 = struct.unpack('<qBBHhH', header)
+                packet = {
+                    'time': datetime.fromtimestamp(tstamp / 1000),
+                    'direction': direction, #'->' if direction else '<-'
+                    'number': i,
+                    'length': length,
+                    'merged': 1,
+                    'data': file.read(length)
+                }
+                header = file.read(16)
+                i += 1
+                yield packet
 
-def decode_sessions(number_of_sessions):
-    return [decode_session('sess' + str(i+1)) for i in range(number_of_sessions)]
+    def decode_session(self, dirname):
+        session = {
+            'name': dirname,
+            'captures': [{
+                'name': capture,
+                'port': re.match('^[0-9]+_([0-9]+).dat$', capture).group(1),
+                'start_time': datetime.fromtimestamp(int(re.match('^([0-9]+)_[0-9]+.dat$', capture).group(1))/1000),
+                'packets': self.decode_capture(self.capture_iter(os.path.join(dirname, capture)))
+            } for capture in sorted(os.listdir(dirname))]
+        }
 
-# print('')
-# print(sys.argv[1])
-# pprint(decode_session(sys.argv[1]))
-#pprint(decode_sessions(5))
+        session['start_time'] = min(capture['start_time'] for capture in session['captures'])
+        session['number_of_captures'] = len(session['captures'])
 
-dump_file = open('all_decoded.pickle', 'wb')
-pickle.dump(decode_sessions(5), dump_file)
+        return session
+
+    def decode_sessions(self):
+        return [self.decode_session(dir) for dir in self.dirs]
+
+if __name__ == '__main__':
+
+    decoder = AndroidCaptureDecoder(['sess' + str(i+1) for i in range(5)])
+    sessions = decoder.decode_sessions()
+
+    dump_file = open('all_decoded2.pickle', 'wb')
+    pickle.dump(sessions, dump_file)
